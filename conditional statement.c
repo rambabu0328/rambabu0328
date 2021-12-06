b3baaeff24f0a2a0290e231b52faf5972ee14ce8
rambabu0328 <92841692+rambabu0328@users.noreply.github.com>
2021-12-06 10:43:40 +0530

    Add files via upload

---
+++ conditional statement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter a number it is even or odd");
+	scanf("%d",&a);
+	if(a%2 == 0)
+    {
+    	printf("%d the given number is even",a);
+    }
+    else
+    {
+    	printf("%d the given number is odd",a);
+	}
+    
+}
