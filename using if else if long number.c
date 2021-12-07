4cbb504099b5912a9127e1905cf433420c00e4a1
rambabu0328 <92841692+rambabu0328@users.noreply.github.com>
2021-12-07 10:17:57 +0530

    Add files via upload

---
+++ using if else if long number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	scanf("%d",&a,&b);
+	printf("enter a number");
+	if (a<b)
+	{
+		printf("b is larger number");
+	}
+	else
+	{
+		printf("a is smaller number ");
+	}
+
+	
+}
